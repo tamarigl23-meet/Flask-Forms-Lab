e06f497313b636d07d8f93f655cf8d60dffefbb1
tamarigl23-meet <109900070+tamarigl23-meet@users.noreply.github.com>
2022-07-27 17:03:26 +0300

    flask-form lab added

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -7,18 +7,29 @@ app = Flask(  # Create a flask app
 	static_folder='static'  # Name of directory for static files
 )
 
-
-username = "llo2ay"
+username = "Tamari"
 password = "123"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
+@app.route('/', methods=['GET', 'POST'])
+def login_page():
+		if request.method == 'GET':
+				return render_template('login.html')
+		else:
+				username1 = request.form['username']
+				password1 = request.form['password']
 
-@app.route('/')  # '/' for the default page
-def login():
-  return render_template('login.html')
-  
+				if username == username1 and password== password1:
+						return redirect(url_for('home'),
+						user_check = username,
+						pass_check = password)
 
+				else:
+						return render_template('login.html')
 
+@app.route('/home')
+def home_page():
+      return render_template('home.html')
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
